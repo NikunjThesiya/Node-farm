889a1bb7ccb4e3be44e719e8e3b484cbec81b3db
thesiya-nikunj <wd.nikunj@gmail.com>
2020-10-02 12:21:51 +0530

    node farm completed

---
+++ index.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
+
+const tempOverview = fs.readFileSync(
+	`${__dirname}/templates/template-overview.html`,
+	"utf-8"
+);
+const tempCard = fs.readFileSync(
+	`${__dirname}/templates/template-card.html`,
+	"utf-8"
+);
+const tempProduct = fs.readFileSync(
+	`${__dirname}/templates/template-product.html`,
+	"utf-8"
+);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+	// console.log(req);
+	// console.log(req.url);
+	const { query, pathname } = url.parse(req.url, true);
+	const pathName = req.url;
+
+	if (pathname === "/" || pathname === "/overview") {
+		res.writeHead(200, { "Content-type": "text/html" });
+		const cardsHtml = dataObj
+			.map((el) => replaceTemplate(tempCard, el))
+			.join("");
+		const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+		res.end(output);
+	} else if (pathname === "/product") {
+		// console.log(query);
+		const product = dataObj[query.id];
+		const output = replaceTemplate(tempProduct, product);
+		res.end(output);
+	} else if (pathname === "/api") {
+		fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+			res.writeHead(200, { "Content-type": "application/json" });
+			// console.log(productData);
+			res.end(data);
+		});
+	} else {
+		res.writeHead(404, {
+			"content-type": "text/html",
+			"my-own-header": "hello world",
+		});
+		res.end("<h1>This Page could not be found</h1>");
+	}
+});
+
+server.listen(8000, "127.0.0.1", () => {
+	console.log("Listening to server on port 8000");
+});
