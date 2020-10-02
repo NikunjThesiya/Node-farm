e7698af48df115bbd15ea4b114f9ef744db2a3b2
thesiya-nikunj <wd.nikunj@gmail.com>
2020-10-02 14:06:22 +0530

    nodemon package installed and used

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
 
 const tempOverview = fs.readFileSync(
@@ -17,6 +18,10 @@ const tempProduct = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
 	// console.log(req);
